40c9c4c6eb8779957cee0cf754d526f5b2aa570d
Masaki Gogami <moumoon921yahhh@gmail.com>
2022-01-10 13:26:46 +0900

    Update install_node.sh

--- install_node.sh
+++ install_node.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-sudo apt install -y nodejs npm
+sudo apt-get install -y nodejs npm
 sudo npm install n -g
 sudo n stable
-sudo apt purge -y nodejs npm
+sudo apt-get purge -y nodejs npm
 exec $SHELL -l
 node -v
