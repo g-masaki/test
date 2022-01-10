2627195bb2bfbedad459d64f8062b379e30cda21
Masaki Gogami <moumoon921yahhh@gmail.com>
2022-01-11 01:22:57 +0900

    Update install_node.sh

--- install_node.sh
+++ install_node.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
-curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-sudo apt-get install -y nodejs npm
+
+# Install nodejs
+curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt-get install -y nodejs
+# Install nodejs with specific version 
 sudo npm install n -g
-sudo n stable
+sudo n 14.18.1
+
+# Remove LTS version
 sudo apt-get purge -y nodejs npm
+
+# restart shell
 exec $SHELL -l
+
+#  
 node -v
+
+#https://www.digitalocean.com/community/tutorials/how-to-install-node-js-on-ubuntu-20-04-ja
